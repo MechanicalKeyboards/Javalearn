912fdf8ec564ee93eea9f0f701b7b0b1c843c9f3
Mark <mmopalinski@cps.edu>
2017-07-11 21:10:34 -0500

    [lesson 1] CntrPrntName

--- src/com/javalearn/lesson1/Main.java
+++ src/com/javalearn/lesson1/Main.java
@@ -4,4 +4,7 @@ package com.javalearn.lesson1;
  * Created by Bob on 6/27/2017.
  */
 public class Main {
+    public static void main(String[] param) {
+        System.out.println("\t\t\tMark");
+    }
 }
