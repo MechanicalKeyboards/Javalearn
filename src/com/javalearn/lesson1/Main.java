88111cdd72038ec42ba433b7eea08132c190f15f
Mark <mmopalinski@cps.edu>
2017-07-19 20:19:40 -0500

    Lesson3 Question1

--- src/com/javalearn/lesson1/Main.java
+++ src/com/javalearn/lesson1/Main.java
@@ -3,14 +3,16 @@ package com.javalearn.lesson1;
 /**
  * Created by Bob on 6/27/2017.
  */
+
 import java.util.Scanner;
-class Main{
-    public static void main(String args[]){
-        Scanner sc=new Scanner(System.in);
+
+class Main {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
 
         System.out.println("Enter your name");
-        String name=sc.next();
-        System.out.println("Hi"+name+", nice to see you.");
+        String name = sc.next();
+        System.out.println("Hi " + name + ", nice to see you.");
         sc.close();
     }
 }
