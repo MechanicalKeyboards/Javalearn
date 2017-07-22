c89e0cb0b4bf71cf8d6a4c63245ea16b555c0e78
Mark <mmopalinski@cps.edu>
2017-07-22 09:12:17 -0500

    Lesson2 Question2

---
+++ src/com/javalearn/unit1/lesson2/Activity2.java
@@ -0,0 +1,11 @@
+package com.javalearn.unit1.lesson2;
+
+/**
+ * Created by Bob on 7/20/2017.
+ */
+public class Activity2 {
+    public static void main(String[] args) {
+        System.out.println("\"I do not fear computers. I fear the lack of them.\"\nIsaac Asimov");
+    }
+}
+
