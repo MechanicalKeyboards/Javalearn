2c0c24b52838c890d0c60417bde4491f59df477c
Mark <mmopalinski@cps.edu>
2017-07-22 09:12:38 -0500

    Lesson2 Question3

---
+++ src/com/javalearn/unit1/lesson2/Activity3.java
@@ -0,0 +1,9 @@
+package com.javalearn.unit1.lesson2;
+
+/**
+ * Created by Bob on 7/20/2017.
+ */
+public class Activity3 {
+    public static void main(String[] args) {
+                System.out.println("   _\n /   \\\n|     |\n \\ _ /");
+}}
