7c120c520618220102db6fe695728d518cb27cde
Mark <mmopalinski@cps.edu>
2017-07-22 09:10:08 -0500

    Lesson1 Question2

---
+++ src/com/javalearn/unit1/lesson1/Activity2.java
@@ -0,0 +1,10 @@
+package com.javalearn.unit1.lesson1;
+
+/**
+ * Created by Bob on 7/20/2017.
+ */
+public class Activity2 {
+    public static void main(String args[]) {
+        System.out.println("\t\t\tMark\n\t\t\tMark\n\t\t\tMark");
+    }
+}
