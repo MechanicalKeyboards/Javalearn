224aa4449e98010386d608803669d878e6812109
Mark <mmopalinski@cps.edu>
2017-07-23 18:35:55 -0500

    Lesson3 Question3

---
+++ src/com/javalearn/unit1/lesson3/Activity3.java
@@ -0,0 +1,18 @@
+package com.javalearn.unit1.lesson3;
+
+/**
+ * Created by Bob on 7/23/2017.
+ */
+import java.util.Scanner;
+public class Activity3 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your name");
+        String name = sc.next();
+        System.out.println("Enter an adjective describing you");
+        String adj = sc.next();
+        System.out.println("My name is " + name + ", I am " + adj );
+
+
+    }
+}
