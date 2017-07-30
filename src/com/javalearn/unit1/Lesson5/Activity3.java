2b27ec0b352402ec563dbc5404414ce37d582c96
MechanicalKeyboards <mmopalinski@cps.edu>
2017-07-30 09:34:03 -0500

    Lesson5 Question3

---
+++ src/com/javalearn/unit1/Lesson5/Activity3.java
@@ -0,0 +1,14 @@
+package com.javalearn.unit1.Lesson5;
+import java.util.Scanner;
+public class Activity3 {
+    public static void main(String args[]){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the radius of a circle");
+        float radius = scan.nextFloat();
+        double area = 2 * 3.14159265 * radius;
+        double circumference = 3.14159265 * radius ^ 2.0;
+        System.out.println("The area is: " + area + "The circumference is: " + circumference);
+
+
+    }
+}
